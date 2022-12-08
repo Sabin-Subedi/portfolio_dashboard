970363aec439b98fb454fa9bb620daddcb615871
Sabin Subedi <sabinsubedi125@gmail.com>
2022-12-08 13:38:00 +0545

    added the 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,49 @@
+import { Box, Button, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { errorLayout } from "./layout/error";
 
 function Custom404() {
-  return <div>Custom404</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Typography variant="h3" color="grey.800" align="center" mb={3}>
+        Sorry, page not found!
+      </Typography>
+      <Typography
+        variant="caption"
+        fontSize={16}
+        mb={5}
+        color="grey.600"
+        sx={{
+          maxWidth: "25rem",
+        }}
+        align="center"
+      >
+        Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve
+        mistyped the URL? Be sure to check your spelling.
+      </Typography>
+      <Image
+        src="/404.svg"
+        alt="404"
+        width={350}
+        height={350}
+        objectFit="contain"
+      />
+      <Link href="/" passHref>
+        <Button sx={{ mt: 2 }} color="primary" variant="contained" size="large">
+          Go To Home
+        </Button>
+      </Link>
+    </Box>
+  );
 }
 
 Custom404.getLayout = errorLayout;
--- pages/layout/error.js
+++ pages/layout/error.js
@@ -3,7 +3,19 @@ import Navbar from "../../components/Navbar";
 import SideNav from "../../components/SideNav";
 
 const Layout = ({ children }) => {
-  return <Box sx={{ display: "flex", height: "100vh" }}>{children}</Box>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: "100vh",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {children}
+    </Box>
+  );
 };
 
 export const errorLayout = (page) => <Layout>{page}</Layout>;
